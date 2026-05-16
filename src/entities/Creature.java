9ab0acb9c073ef4b0b5b81f54681192bf05a5dad
Nssstiis <nssstiis@mail.ru>
2026-05-16 16:52:50 +0300

    refactor: add canEat abstraction and update makeMove parameters

--- src/entities/Creature.java
+++ src/entities/Creature.java
@@ -1,10 +1,13 @@
 package entities;
 
+import world.Position;
+import world.WorldMap;
+
 public abstract class Creature extends Entity {
     public int speed;
     public int hp;
 
+    public abstract void makeMove(WorldMap worldMap, Position currentPosition);
 
-
-    public abstract void makeMove();
+    public abstract boolean canEat(Entity entity);
 }
