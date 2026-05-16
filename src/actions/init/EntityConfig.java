f80090874be6abaab248c57c7f1548709a1dd984
Nssstiis <nssstiis@mail.ru>
2026-05-16 17:03:35 +0300

    refactor: convert EntityConfig to record

--- src/actions/init/EntityConfig.java
+++ src/actions/init/EntityConfig.java
@@ -1,37 +1,4 @@
 package actions.init;
 
-public class EntityConfig {
-    private final int predatorCount;
-    private final int preyCount;
-    private final int coralCount;
-    private final int islandCount;
-    private final int waveCount;
-
-    public EntityConfig(int predatorCount, int preyCount, int coralCount, int islandCount, int waveCount) {
-        this.predatorCount = predatorCount;
-        this.preyCount = preyCount;
-        this.coralCount = coralCount;
-        this.islandCount = islandCount;
-        this.waveCount = waveCount;
-    }
-
-    public int getPredatorCount() {
-        return predatorCount;
-    }
-
-    public int getPreyCount() {
-        return preyCount;
-    }
-
-    public int getCoralCount() {
-        return coralCount;
-    }
-
-    public int getIslandCount() {
-        return islandCount;
-    }
-
-    public int getWaveCount() {
-        return waveCount;
-    }
+public record EntityConfig(int predatorCount, int preyCount, int coralCount, int islandCount, int waveCount) {
 }
