43d49ebc84bb80722c83487ebf4093b05ac682a9
Nssstiis <nssstiis@mail.ru>
2026-05-18 19:53:59 +0300

    refactor: clean up unused code

--- src/actions/init/EntityPlacerAction.java
+++ src/actions/init/EntityPlacerAction.java
@@ -1,13 +1,12 @@
 package actions.init;
 
-import actions.Action;
 import entities.Entity;
 import world.Position;
 import world.WorldMap;
 
 import java.util.List;
 
-public class EntityPlacerAction extends Action {
+public class EntityPlacerAction {
 
     public static void placeEntities(List<Entity> entities, WorldMap worldMap) {
         for (Entity entity : entities) {
