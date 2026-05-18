ca4b4bd519b4c66e026d29ad6f1ec17d5f5feaa7
Nssstiis <nssstiis@mail.ru>
2026-05-18 14:46:18 +0300

    fix: prevent creatures from moving twice

--- src/actions/turn/CreatureMovementAction.java
+++ src/actions/turn/CreatureMovementAction.java
@@ -7,25 +7,34 @@ import world.Position;
 import world.WorldMap;
 
 import java.util.ArrayDeque;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
+import java.util.Set;
 
 public class CreatureMovementAction {
     Queue<Position> creaturesPositions = new ArrayDeque<>();
+    Set<Creature> movedCreature = new HashSet<>();
 
     public void makeCreaturesMove(WorldMap worldMap) {
         creaturesPositions.clear();
+        movedCreature.clear();
         collectCreaturesPositions(worldMap);
 
         for (Position currentPosition : creaturesPositions) {
             Creature currentEntity = (Creature) worldMap.getEntityAt(currentPosition);
+
+            if (movedCreature.contains(currentEntity)) {
+                continue;
+            }
 
             if (currentEntity == null) {
                 continue;
             }
 
             currentEntity.makeMove(worldMap, currentPosition);
+            movedCreature.add(currentEntity);
         }
     }
 
